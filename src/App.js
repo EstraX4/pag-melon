ce6f61cc4480ec85e5f772aeebb07cd8101f121e
Juan David Estrada Carvajal <Estraxclips@gmail.com>
2023-11-03 23:14:26 -0500

    create NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+      <div className="container">
+      <NavBar />
+      {/* Agrega el contenido de tu aplicación aquí */}
     </div>
-  );
-}
+    );
+  }
+
 
 export default App;
